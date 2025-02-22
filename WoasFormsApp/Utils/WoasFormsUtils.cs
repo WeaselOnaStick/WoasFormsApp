4928f8c40b49fc32b894f84a8a099c1d8327d171
Weasel On A Stick <korotkih.00@gmail.com>
2025-02-23 02:37:54 +0400

    first localization batch (en+ru)

--- WoasFormsApp/Utils/WoasFormsUtils.cs
+++ WoasFormsApp/Utils/WoasFormsUtils.cs
@@ -1,15 +1,14 @@
-﻿using WoasFormsApp.Data;
+﻿using Microsoft.Extensions.Localization;
+using WoasFormsApp.Data;
 
 namespace WoasFormsApp.Utils
 {
     public static class WoasFormsUtils
     {
-        private static string TemplateFallbackTitle = "Unnamed Template (ID {0})";
-
-        public static string TemplateTitleOrUnnamed(Template template, string? fallback = null)
+        public static string TemplateTitleOrUnnamed(Template template, IStringLocalizer loc)
         {
-            fallback ??= TemplateFallbackTitle;
-            return string.IsNullOrWhiteSpace(template.Title) ? string.Format(fallback, template.Id) : $"\"{template.Title}\"";
+            var fallback = loc["UNNAMED_TEMPLATE_ID", template.Id];
+            return string.IsNullOrWhiteSpace(template.Title) ? fallback : $"\"{template.Title}\"";
         }
     }
 }
